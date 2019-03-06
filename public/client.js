39f5bbefd591360e338d1a0d4e710ac6f13009cd
Glitch (sivonne-mejo583-marvel-project) <none>
2019-03-06 16:05:34 +0000

    🌑👠 Checkpoint ./public/client.js:3109007/168 ./views/index.html:3109007/245

--- public/client.js
+++ public/client.js
@@ -1,9 +1,11 @@
+//modelled after the Spotify API Assignment
 document.addEventListener('DOMContentLoaded', function() {
-//from https://developer.marvel.com/docs#!/public/getComicCharacterCollection_get_8
+//link from https://developer.marvel.com/docs#!/public/getComicCharacterCollection_get_8
 fetch('https://gateway.marvel.com:443/v1/public/characters?apikey=5a9b29e7ed2141b0cda10a06ec71d3a5').then(resp => resp.json()).then((data) => {
     console.group('%cResponse from /characters', 'color: #F037A5; font-size: large');
     console.log(data);
     console.groupEnd();
  
 });
+  
 });
