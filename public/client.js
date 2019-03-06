2b71efde49343a361b7733ac73f6f51c0a1d0a97
Glitch (sivonne-mejo583-marvel-project) <none>
2019-03-06 18:37:33 +0000

    😽👄 Checkpoint ./public/client.js:3109007/2718 ./views/index.html:3109007/2605 ./public/style.css:3109007/219

--- public/client.js
+++ public/client.js
@@ -11,19 +11,39 @@ fetch('/series').then(resp => resp.json()).then((data) => {
   
 
   
-  // init controller
-var controller = new ScrollMagic.Controller();
+//   // init controller
+// var controller = new ScrollMagic.Controller();
 
-// create a scene
-new ScrollMagic.Scene({
-        duration: 100,    // the scene should last for a scroll distance of 100px
-        offset: 50    // start this scene after scrolling for 50px
-    })
-    .setPin("#sect-1") // pins the element for the the scene's duration
-    .addTo(controller); // assign the scene to the controller
-  
-  
-  
-});
+// // create a scene
+// new ScrollMagic.Scene({
+//         duration: 100,    // the scene should last for a scroll distance of 100px
+//         offset: 50    // start this scene after scrolling for 50px
+//     })
+//     .setPin("#sect-1") // pins the element for the the scene's duration
+//     .addTo(controller); // assign the scene to the controller
 
+
+  
+// });
+$(function () { // wait for document ready
+		// init
+		var controller = new ScrollMagic.Controller({
+			globalSceneOptions: {
+				triggerHook: 'onLeave'
+			}
+		});
+
+		// get all slides
+		var slides = document.querySelectorAll("section.panel");
+
+		// create scene for every slide
+		for (var i=0; i<slides.length; i++) {
+			new ScrollMagic.Scene({
+					triggerElement: slides[i]
+				})
+				.setPin(slides[i])
+				.addIndicators() // add indicators (requires plugin)
+				.addTo(controller);
+		}
+	});
 
