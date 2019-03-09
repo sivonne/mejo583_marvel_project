1259fda52877fc6fbe31d2efef86c7391fd67fd2
Glitch (sivonne-mejo583-marvel-project) <none>
2019-03-09 05:02:09 +0000

    trying to connect graph to console logged data, still returning errors

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+
+
 // server.js
 // where your node app starts
 
@@ -45,7 +47,7 @@ var marvel = api.createClient({
 app.get('/series/x-men/characters', function (request, response) {
   // Code from Resource https://www.npmjs.com/package/marvel-api
 //looping through data
-for(var i = 0; i < 100; i++){
+// for(let i = 0; i < 99; i++){
   marvel.characters.findAll(limit = 99)//come back to this later
   .then(function(data) {
     
@@ -55,9 +57,31 @@ for(var i = 0; i < 100; i++){
     
   .fail(console.error)
   .done();
-  
-}
-});
+// }
+  var MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+		var color = Chart.helpers.color;
+		var barChartData = {
+			labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+			datasets: [{
+				label: 'Dataset 1',
+				backgroundColor: color(window.chartColors.red).alpha(0.5).rgbString(),
+				borderColor: window.chartColors.red,
+				borderWidth: 1,
+				data: [marvel.characters.findAll(limit = 99)
+				]
+			}, {
+				label: 'Dataset 2',
+				backgroundColor: color(window.chartColors.blue).alpha(0.5).rgbString(),
+				borderColor: window.chartColors.blue,
+				borderWidth: 1,
+				data: [
+					marvel.characters.comics(limit = 99)
+				]
+			}]
+
+		};
+			window.myBar.update();
+		});
   
   
   
