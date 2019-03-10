c7ff5f5376151544942199f8045842934855682d
Glitch (sivonne-mejo583-marvel-project) <none>
2019-03-10 02:58:11 +0000

    🐕💇 Checkpoint ./public/style.css:3109007/1155 ./server.js:3109007/103

--- server.js
+++ server.js
@@ -48,9 +48,10 @@ app.get('/series/x-men/characters', function (request, response) {
   // Code from Resource https://www.npmjs.com/package/marvel-api
 //looping through data
 // for(let i = 0; i < characters.length; i++){
-  marvel.characters.findAll(limit = 99)//come back to this later
+  marvel.characters.findAll()//come back to this later
   .then(function(data) {
       response.send(data);
+    limit++;
     
        })
   
